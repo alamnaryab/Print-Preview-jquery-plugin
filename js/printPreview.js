06186064842c90deb37e0619aff54ebda6b0a429
Alam <alamnaryab@ymail.com>
2015-08-23 22:15:39 +0500

    multiple seperate window and input values

--- js/printPreview.js
+++ js/printPreview.js
@@ -1,1 +1,50 @@
-!function(t){t.fn.printPreview=function(e){var i=this,r=t.extend({obj2print:"body",style:"",width:"670",height:screen.height-105,top:0,left:"center",resizable:"yes",scrollbars:"yes",status:"no",title:"Print Preview"},e);return"center"==r.left&&(r.left=screen.width/2-r.width/2),i.bind("click.printPreview",function(){var e=i[0].outerHTML,n="";n=t("head").html();var l="<!DOCTYPE html><html><head>"+n+r.style+"</head><body>";l+=t(r.obj2print)[0].outerHTML.replace(e,"")+"</body></html>";var d = new Date();var n = 'newWindow'+d.getMilliseconds();var o=window.open("",n,"width="+r.width+",top="+r.top+",height="+r.height+",left="+r.left+",resizable="+r.resizable+",scrollbars="+r.scrollbars+",status="+r.status);o.document.write(l),o.document.title=r.title})}}(jQuery);
+;(function ( $ ) {
+    $.fn.printPreview = function( options ) {
+        var elem = this;
+        
+        var opt = $.extend({
+            obj2print:'body',
+            style:'',
+            width:'670',
+            height:screen.height-105,
+            top:0,
+            left:'center',
+            resizable : 'yes',
+            scrollbars:'yes',
+            status:'no',
+            title:'Print Preview'
+        }, options );
+        if(opt.left == 'center'){
+            opt.left=(screen.width/2)-(opt.width/2);
+        }
+        $(opt.obj2print+" input").each(function(){
+            $(this).attr('value',$(this).val());
+        });
+        $(opt.obj2print+" textarea").each(function(){
+            $(this).html($(this).val());
+        });
+        return elem.bind("click.printPreview", function () {
+            var btnCode = elem[0].outerHTML;
+            var headString = '';
+            headString = $("head").html();
+            var str = "<!DOCTYPE html><html><head>"+headString+opt.style+"</head><body>";
+            str+=$(opt.obj2print)[0].outerHTML.replace(btnCode,'')+"</body></html>";
+            //top open multiple instances we have to name newWindow differently, so getting milliseconds
+            var d = new Date();
+            var n = 'newWindow'+d.getMilliseconds();
+            var newWindow = window.open(
+                    "", 
+                    n, 
+                    "width="+opt.width+
+                    ",top="+opt.top+
+                    ",height="+opt.height+
+                    ",left="+opt.left+
+                    ",resizable="+opt.resizable+
+                    ",scrollbars="+opt.scrollbars+
+                    ",status="+opt.status
+                    );
+            newWindow.document.write(str);
+            newWindow.document.title = opt.title;
+        });
+    };
+}( jQuery ));
